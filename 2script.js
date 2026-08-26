ebc6c269f0e93220f55012680f22be3dbb58f086
pauloneumann <paulo.baptistel.alves@escola.pr.gov.br>
2026-08-25 21:56:46 -0300

    Add files via upload

---
+++ 2script.js
@@ -0,0 +1,7 @@
+// Arquivo de apoio. O envio real dos dados será estudado em exercícios posteriores
+const formulario = documento. querySelector('form');
+formulario?.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.info('Formulário válido e pronto para integração futura.');
+});
+console.info('Exercício 02 carregado.');
